cc231f39ad3dbda0a4182852bba73bf0de267bae
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-10-02 17:16:29 -0500

    DB Drill: AR Student Schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,20 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validate :validate_email
+  validates :age, numericality: { greater_than: 5 }
+  validate :validate_phone_number
+
+  def validate_email
+    self.errors.add :email, 'Invalid email' unless email =~ /.*[@]+.+[.]..+/
+  end
+
+  def validate_phone_number
+    self.errors.add :phone, 'Invalid phone number' unless phone =~ /\(?\d*\)?\s\d*\-\d*(\sx\d*)?/
+  end
+
   def name
     "#{first_name} #{last_name}"
   end
